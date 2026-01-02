79c9b0046fb322a816fdde4d70cb610d0d1a46d2
ext-mapicco_meli <mauriscode@gmail.com>
2026-01-02 18:40:47 -0300

    basic data types and variables

---
+++ cmd/scaffold/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	var shornum int16 = 5
+	var longnum int64 = 106376373
+
+	var myUint uint = 34 // can store more numbers than int but only positive
+
+	var float float32 = 34.567
+
+	fmt.Println("Lets Go!!!!")
+
+	var myString string = "Hello go" // one liner
+	fmt.Println(myString)
+
+	var anotherString string
+	anotherString = ` this is a 
+	multi line string`
+
+	fmt.Println(len(anotherString))
+	/* IMPORTANTE: len me devuelve la cantidad de BYTES, no de caracteres.
+	Un caracter no ASCII puede ocupar mas de un byte
+	*/
+	fmt.Println(shornum, longnum, myUint, float)
+
+	var myBool bool = true
+	fmt.Println(myBool)
+
+	/* 
+	defaults: 0, false
+	*/
+
+	var str1 = "Hello" // inferred type
+	str2 := "World" //shorter declaration with inferred type
+
+	/* CONST */
+	const myConst string = "I am constant" //tengo que declararla en el momento con el valor. Constantes son inmutables
+
+
+}
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module lets_go
+
+go 1.25.5
