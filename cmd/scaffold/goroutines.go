ecf61945c95444bbadddde31d786614ed35c8d5e
ext-mapicco_meli <ext_mapicco@mercadolibre.com>
2026-01-12 11:30:46 -0300

    go routines/locks

---
+++ cmd/scaffold/goroutines.go
@@ -0,0 +1,38 @@
+//go routines = way to launch multiple functions and have them executing concurrently
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+var m = sync.Mutex{}
+var wg = sync.WaitGroup{}
+var dbData = []string{"id1", "id2", "id3", "id4", "id5"}
+var results = []string{}
+
+
+func main() {
+	t0 := time.Now()
+	for i:=0; i<len(dbData); i++ {
+		wg.Add(1)
+		go dbCall(i) //launch the function as a goroutine
+	}
+	wg.Wait()
+	fmt.Printf("Total time: %v\n", time.Since(t0))
+	fmt.Printf("Results: %v\n", results)
+}
+
+
+func dbCall(i int) {
+	var delay float32 = rand.Float32()*2000
+	time.Sleep(time.Duration(delay) * time.Millisecond)
+	fmt.Println("Result from de database:", dbData[i])
+
+	m.Lock() //mutex lock
+	results = append(results, dbData[i])
+	m.Unlock()
+	wg.Done()
+}
--- cmd/scaffold/pointers.go
+++ cmd/scaffold/pointers.go
@@ -6,8 +6,19 @@ func main() {
 	var p *int32 = new(int32) // el new le devuelve una direccion de memoria
 	var i int32
 
-	fmt.Println("Valor de p:", p)   // direccion de memoria
+	fmt.Println("Valor de p:", p)   // direccion de memoria ej 0x1400011c004
 	fmt.Println("Valor de *p:", *p) // dereferencia al valor (0) (preguntando lo que efectivamente hay en la direccion de memoria que tiene el puntero)
 	fmt.Println("Valor de i:", i)    // valor por defecto (0)
+
+	*p = 10
+	fmt.Println("Valor de *p despues de asignar 10:", *p) // dereferencia al valor (10)
+
+	p = &i //ahora el puntero apunta a la direccion de i
+	*p = 25  //cambio el valor de i a traves del puntero
+	fmt.Println("Valor de p despues de asignar la direccion de i:", i) // valor de i (25)
+
+
+	//useful when we use large parameters so we dont have to copy the intire data
+
 }
 
