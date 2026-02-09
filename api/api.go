56d46dc5a3724d104b8e8e2c5e03714efe3ef446
mauriciomolinapicco <mauriscode@gmail.com>
2026-02-09 16:00:56 -0300

    set api 2

--- api/api.go
+++ api/api.go
@@ -19,3 +19,26 @@ type Error struct {
 	Code int
 	Message string //error message
 }
+
+//function that gives the error to user
+func writeError(w http.ResponseWriter, message string, code int) {
+	resp := Error{
+		Code: code,
+		Message: message,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(resp)
+}
+
+//func wrappers
+var (
+	RequestErrorHandler = func(w http.ResponseWriter, err error) {
+		writeError(w, err.Error(), http.StatusBadRequest)
+	}
+
+	InternalErrorHandler = func(w http.ResponseWriter, err error) {
+		writeError(w, "internal server error", http.StatusInternalServerError)
+	}
+)
---
+++ api/internal/middleware/authorization.go
@@ -0,0 +1,45 @@
+package middleware
+
+import (
+	"net/http"
+	"errors"
+	"github.com/mauriciomolinapicco/lets-go/api/api"
+	"github.com/mauriciomolinapicco/lets-go/api/internal/tools"
+
+	log "github.com/sirupsen/logrus"
+
+)
+
+var UnAuthorizedError = errors.New("invalid user or token")
+
+func Authorization(next http.Handler) http.Handler {
+	//        					(response writer, pointer to request)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		
+		var username string = r.URL.Query().Get("username")
+		var token = r.Header.Get("Authorization")
+
+		if username == "" || token == "" {
+			log.Error(UnAuthorizedError)
+			api.RequestErrorHandler(w, UnAuthorizedError)
+			return
+		}
+
+		//instanciar puntero a db
+		var database *tools.DatabaseInterface
+		database, err = toolsNewDatabase()
+		if err != nil {
+			api.InternalErrorHandler(w, err)
+			return
+		}
+
+		var loginDetails *tools.loginDetails
+		loginDetails = (*database).GetUserLoginDetails(username)
+
+		if (loginDetails == nil || (token != (*loginDetails).AuthToken)) {
+			log.Error(UnAuthorizedError)
+			api.RequestErrorHandler(w, UnAuthorizedError)
+			return
+	}
+}
+
